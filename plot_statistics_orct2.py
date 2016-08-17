db6262cd8f1bc24817bf2a12f2d548ca0f25fa0b
brecht <brecht_de_meulenaere@outlook.com>
2016-08-18 01:37:17 +0200

    Removed unnecessary inkt from plots

--- plot_statistics_orct2.py
+++ plot_statistics_orct2.py
@@ -26,7 +26,7 @@ def _plot(data, title, yaxis_title, filename):
     # Replace the target strings
     tempHTML = tempHTML.replace('displayModeBar:!0', 'displayModeBar:!1')
     tempHTML = tempHTML.replace('displaylogo:!0', 'displaylogo:!1')
-    tempHTML = tempHTML.replace('modeBarButtonsToRemove:[]', 'modeBarButtonsToRemove:["toImage","sendDataToCloud","zoom2d", "pan2d", "select2d", "lasso2d", "zoomIn2d", "zoomOut2d", "autoScale2d", "resetScale2d", "hoverClosestCartesian", "hoverCompareCartesian"]')
+    tempHTML = tempHTML.replace('modeBarButtonsToRemove:[]', 'modeBarButtonsToRemove:["toImage","sendDataToCloud","zoom2d", "pan2d", "select2d", "lasso2d", "zoomIn2d", "zoomOut2d", "autoScale2d", "hoverClosestCartesian", "hoverCompareCartesian", "hoverClosestGl2d"]')
 
     with open(filename, 'w') as file:
         file.write(tempHTML)
@@ -35,22 +35,28 @@ def _plot(data, title, yaxis_title, filename):
     os.remove("temp-plot.html")
 
 def plot_all():
-    count_global = go.Scatter(
+    count_global = go.Scattergl(
         x=count_data["timestamp"], 
         y=count_data["count_global"],
-        name="RCT2_GLOBAL"
+        name="RCT2_GLOBAL",
+    	mode = 'markers', 
+        marker=dict(size=2)
     )
     
-    count_call = go.Scatter(
+    count_call = go.Scattergl(
         x=count_data["timestamp"], 
         y=count_data["count_call"],
-        name="RCT2_CALL"
+        name="RCT2_CALL",
+    	mode = 'markers', 
+        marker=dict(size=2)
     )
     
-    count_address = go.Scatter(
+    count_address = go.Scattergl(
         x=count_data["timestamp"], 
         y=count_data["count_address"],
-        name="RCT2_ADDRESS"
+        name="RCT2_ADDRESS",
+    	mode = 'markers', 
+        marker=dict(size=2)
     )
     
     data = [
@@ -59,22 +65,61 @@ def plot_all():
         count_global
     ]
     
+    count_global_2 = go.Scatter(
+        x=count_data["timestamp"], 
+        y=count_data["count_global"],
+        name="RCT2_GLOBAL"
+    )
+    
+    count_call_2 = go.Scatter(
+        x=count_data["timestamp"], 
+        y=count_data["count_call"],
+        name="RCT2_CALL"
+    )
+    
+    count_address_2 = go.Scatter(
+        x=count_data["timestamp"], 
+        y=count_data["count_address"],
+        name="RCT2_ADDRESS"
+    )
+    
+    data_2 = [
+        count_address_2,
+        count_call_2,
+        count_global_2
+    ]
+    
     title = "Plot of RCT2_ADDRESS, RCT2_CALL, and RCT2_GLOBAL occurrences per commit"
     yaxis_title = "Number of occurrences"
     filename = "orct2_graph.html"
+    filename_2 = "orct2_graph_2.html"
     
     _plot(data, title, yaxis_title, filename)
+    _plot(data_2, title, yaxis_title, filename_2)
     
     # Matplotlib
     plt.figure(figsize=(20,10))
     plt.ylabel('Number of occurrences')
     plt.xlabel('Timestamp of commit')
     plt.title("Plot of RCT2_ADDRESS, RCT2_CALL, and RCT2_GLOBAL occurrences per commit")
-    plt.grid(True)
-    plt_address, = plt.plot(count_data["timestamp"], count_data["count_address"])
-    plt_call, = plt.plot(count_data["timestamp"], count_data["count_call"])
-    plt_global, = plt.plot(count_data["timestamp"], count_data["count_global"])
-    plt.legend([plt_address, plt_call, plt_global], ["RCT2_ADRESS", "RCT2_CALL", "RCT2_GLOBAL"], loc=2)
+    #plt.grid(True)
+    plt_address, = plt.plot_date(count_data["timestamp"], count_data["count_address"], ".")
+    plt_call, = plt.plot_date(count_data["timestamp"], count_data["count_call"], ".")
+    plt_global, = plt.plot_date(count_data["timestamp"], count_data["count_global"], ".")
+    leg = plt.legend([plt_address, plt_call, plt_global], ["RCT2_ADRESS", "RCT2_CALL", "RCT2_GLOBAL"], loc=2)
+    leg.get_frame().set_linewidth(0.0)
+    
+    ax = plt.gca()
+    ax.spines['top'].set_visible(False)
+    ax.spines['right'].set_visible(False)
+    ax.spines['bottom'].set_visible(False)
+    ax.spines['left'].set_visible(False)
+    ax.get_xaxis().tick_bottom()    
+    ax.get_yaxis().tick_left()
+    plt.tick_params(axis="both", which="both", bottom="off", top="off", labelbottom="on", left="off", right="off", labelleft="on")
+    
+    ax.yaxis.grid(True)  
+    
     #plt.show()
     plt.savefig("plt_all.pdf")
     plt.savefig("plt_all.png")
@@ -85,35 +130,61 @@ def _plot_plt(data_x, data_y, title, filename):
     plt.ylabel('Number of occurrences')
     plt.xlabel('Timestamp of commit')
     plt.title(title)
-    plt.grid(True)
-    plt.plot(data_x, data_y)
-    #plt.show()
+    #plt.grid(True)
+    
+    ax = plt.gca()
+    ax.spines['top'].set_visible(False)
+    ax.spines['right'].set_visible(False)
+    ax.spines['bottom'].set_visible(False)
+    ax.spines['left'].set_visible(False)
+    ax.get_xaxis().tick_bottom()    
+    ax.get_yaxis().tick_left()
+    plt.tick_params(axis="both", which="both", bottom="off", top="off", labelbottom="on", left="off", right="off", labelleft="on")
+    
+    ax.yaxis.grid(True)  
+    
+    plt.plot_date(data_x, data_y, ".")
     plt.savefig(filename + ".pdf")
     plt.savefig(filename + ".png")
 
 def plot_global():
     data = [
+        go.Scattergl(x=count_data["timestamp"], y=count_data["count_global"], mode = 'markers', marker=dict(size=2))
+    ]
+    
+    data_2 = [
         go.Scatter(x=count_data["timestamp"], y=count_data["count_global"])
     ]
     
     _plot_plt(count_data["timestamp"], count_data["count_global"], "Plot of RCT2_GLOBAL occurrences per commit", "plt_global")
     _plot(data, "Plot of RCT2_GLOBAL occurrences per commit", "Number of occurrences", "orct2_graph_global.html")
+    _plot(data_2, "Plot of RCT2_GLOBAL occurrences per commit", "Number of occurrences", "orct2_graph_global_2.html")
 
 def plot_address():
     data = [
+        go.Scattergl(x=count_data["timestamp"], y=count_data["count_address"], mode = 'markers', marker=dict(size=2))
+    ]
+    
+    data_2 = [
         go.Scatter(x=count_data["timestamp"], y=count_data["count_address"])
     ]
     
     _plot_plt(count_data["timestamp"], count_data["count_address"], "Plot of RCT2_ADDRESS occurrences per commit", "plt_address")
     _plot(data, "Plot of RCT2_ADDRESS occurrences per commit", "Number of occurrences", "orct2_graph_address.html")
+    _plot(data_2, "Plot of RCT2_ADDRESS occurrences per commit", "Number of occurrences", "orct2_graph_address_2.html")
 
 def plot_call():
     data = [
+        go.Scattergl(x=count_data["timestamp"], y=count_data["count_call"], mode = 'markers', marker=dict(size=2))
+    ]
+    
+    data_2 = [
         go.Scatter(x=count_data["timestamp"], y=count_data["count_call"])
     ]
     
     _plot_plt(count_data["timestamp"], count_data["count_call"], "Plot of RCT2_CALL occurrences per commit", "plt_call")
     _plot(data, "Plot of RCT2_CALL occurrences per commit", "Number of occurrences", "orct2_graph_call.html")
+    _plot(data_2, "Plot of RCT2_CALL occurrences per commit", "Number of occurrences", "orct2_graph_call_2.html")
 
 plot_global()
 plot_address()
