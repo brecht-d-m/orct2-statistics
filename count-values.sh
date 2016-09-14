731bd91f52ac70d173e1ad3c6015a0637b27f664
brecht <brecht_de_meulenaere@outlook.com>
2016-09-15 00:08:38 +0200

    Recalculated statistics

--- count-values.sh
+++ count-values.sh
@@ -1,29 +1,41 @@
 #! /bin/bash
 
 output_file="orct2_statistics.csv"
+temp_output_file=$output_file".tmp"
 current_dir=$(pwd)
 cd $1
 
 sep=","
 
 header="commit"$sep"timestamp"$sep"count_global"$sep"count_address"$sep"count_call"
-echo $header > $current_dir"/"$output_file
+touch $current_dir"/"$temp_output_file
+
+if [ ! -e $current_dir"/"$output_file ] ; then
+    echo $header > $current_dir"/"$output_file
+fi
 
 total=$(git rev-list --count develop)
 count=1
 
 for commit in $(git rev-list develop)
 do
+    if grep -q $commit $current_dir"/"$output_file; then
+      break
+    fi
+
     echo -ne $count"/"$total"\r"
     count=$(echo $count+1 | bc)
 
     commit_timestamp=$(git show -s --format=%ci $commit)
     header=$commit$sep$commit_timestamp
-    counts=$(git grep -h -c RCT2_GLOBAL $commit | numsum)$sep$(git grep -h -c RCT2_ADDRESS $commit | numsum)$sep$(git grep -h -c RCT2_CALL $commit | numsum)
-    echo $header$sep$counts >> $current_dir"/"$output_file
+    counts=$(git grep -h -c "RCT2_GLOBAL" $commit | numsum)$sep$(git grep -h -c "RCT2_ADDRESS" $commit | numsum)$sep$(git grep -h -c "RCT2_CALL" $commit | numsum)
+    echo $header$sep$counts >> $current_dir"/"$temp_output_file
 done
 
+cat $current_dir"/"$temp_output_file >> $current_dir"/"$output_file
+
 cd $current_dir
+rm $temp_output_file
 
 python3 plot_statistics_orct2.py
 
